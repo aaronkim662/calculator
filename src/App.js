3fd9f0075054ca6a69beb9eaef829b73e8130570
Aaronkim662 <aaronkim662@gmail.com>
2019-11-03 17:39:46 -0500

    Swtiched from hook to class

--- src/App.js
+++ src/App.js
@@ -2,78 +2,81 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
-  const [number, setNumber] = React.useState({
-    current : ''
-  });
-  const [sign, setSign] = React.useState({
-    sign: ''
-  });
-  const [show, setShow] = React.useState({
-    show: false,
-    value: 0
-  })
+class App extends React.Component {
+  state = {
+    actual : 0,
+    display : '',
+    number: '',
+    show : false,
+    sign : '',
+    total : '',
+  }
 
-  const display = (e) => {
-    e.preventDefault();
-    setShow({show: true})
+  accumulate = (char) => {
+    if (char === '+' || char === '-' || char === '*' || char === '/'){
+      this.setState({
+        actual: 0
+      })
+    }else{
+      this.setState(prevState => ({
+        actual : prevState.actual +parseInt(char)
+      }))
+    }
+    this.setState(prevState => ({
+      display: prevState.display + char
+    }))
 
   }
 
-  const operation = (num) => {
-    if(number.current === ''){
-      setNumber({current : num})
-    }
-    if (sign.sign === '' ){
-      setNumber({current : number.current + num})
-    }
-    if (sign.sign === '+'){
-      setNumber({current: num})
-      setShow({value: parseInt(number.current) + parseInt(num)})
-    }else if (sign.sign === '-'){
-      setShow({value: parseInt(number.current) - parseInt(num)})
-      setNumber({current: ''})
-    }else if (sign.sign === '*'){
-      setShow({value: parseInt(number.current) * parseInt(num)})
-      setNumber({current: ''})
-    }else if (sign.sign === '/'){
-      setShow({value: parseFloat(number.current / parseInt(num))})
-      setNumber({current: ''})
-    }
+  newNumber = () => {
+    this.setState({
+      actual: 0
+    })
   }
 
-  return(
-    <div className='App'>
-    <div className='calcRow'>
-      <h2 onClick={() => operation('1')}>1</h2>
-      <h2 onClick={() => operation('2')}>2</h2>
-      <h2 onClick={() => operation('3')}>3</h2>
-    </div>
-    <div className='calcRow'>
-      <h2 onClick={() => operation('4')}>4</h2>
-      <h2 onClick={() => operation('5')}>5</h2>
-      <h2 onClick={() => operation('6')}>6</h2>
-    </div>
-    <div className='calcRow'>
-      <h2 onClick={() => operation('7')}>7</h2>
-      <h2 onClick={() => operation('8')}>8</h2>
-      <h2 onClick={() => operation('9')}>9</h2>
-    </div>
-    <div className='calcRow'>
-      <h2 onClick={() => setSign({sign: '+'})}>+</h2>
-      <h2 onClick={() => setSign({sign: '-'})}>-</h2>
-      <h2 onClick={() => setSign({sign: '*'})}>*</h2>
-      <h2 onClick={() => setSign({sign: '/'})}>/</h2>
-    </div>
-    <div>
-    <h2>Your value {show.show === true ? show.value : number.current }</h2>
-    <h2>{number.current}</h2>
-    <h2 onClick={(e) => display(e)}>=</h2>
-    <h2 onClick={() => setNumber({current: 0})}>Clear</h2>
-    </div>
-    </div>
+  totaling = () => {
+    this.setState({
+      show : true
+    })
+    const arr = this.state.display.split('+')
+    const sum = parseInt(arr[0]) + parseInt(arr[1])
+    this.setState({
+      total: sum
+    })
+  }
 
-  )
+  render (){
+    return (
+      <div className='App'>
+        <div className='calcRow'>
+          <h2 onClick={() => this.accumulate('1')}>1</h2>
+          <h2 onClick={() => this.accumulate('2')}>2</h2>
+          <h2 onClick={() => this.accumulate('3')}>3</h2>
+        </div>
+        <div className='calcRow'>
+          <h2 onClick={() => this.accumulate('4')}>4</h2>
+          <h2 onClick={() => this.accumulate('5')}>5</h2>
+          <h2 onClick={() => this.accumulate('6')}>6</h2>
+        </div>
+      <div className='calcRow'>
+        <h2 onClick={() => this.accumulate('7')}>7</h2>
+        <h2 onClick={() => this.accumulate('8')}>8</h2>
+        <h2 onClick={() => this.accumulate('9')}>9</h2>
+      </div>
+      <div className='calcRow'>
+        <h2 onClick={() => this.accumulate('+')}>+</h2>
+        <h2 onClick={() => this.accumulate('-')}>-</h2>
+        <h2 onClick={() => this.accumulate('*')}>*</h2>
+        <h2 onClick={() => this.accumulate('/')}>/</h2>
+      </div>
+      <h4>{this.state.display}</h4>
+      <h4 onClick={() => this.totaling()}>=</h4>
+      <h4>{this.state.show ? this.state.total : null}</h4>
+    </div>
+    )
+  }
 }
+
+
 
 export default App;
