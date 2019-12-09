b68d61445735655ed5a13bda64d4d85e520f4325
Aaronkim662 <aaronkim662@gmail.com>
2019-12-08 19:40:22 -0500

    Updated npm packages

--- src/Component/Scientific/Scientific.js
+++ src/Component/Scientific/Scientific.js
@@ -49,6 +49,120 @@ class Scientific extends React.Component {
         // this.arrayIt();
     }
 
+    arrayIt = () => {
+        let str = this.string;
+        let arr = [];
+        let go = true;
+        while(go){
+        for(let i = 0; i < str.length; i+= 1){
+          if(str[i] === '+' || str[i] === '-' || str[i] === '*' || str[i] === '/' || (str[i] === '(') ||str[i] === ')'){
+            let newStr = str.substring(0,i);
+            if(newStr !== ''){
+              arr.push(newStr)
+            }
+            str = str.replace(str.substring(0,i), '');
+    
+            if(str.substring(0,1) !== ''){
+              arr.push(str.substring(0,1))
+            }
+    
+            str = str.replace(str.substring(0,1),'');
+            i = -1;
+            }
+          }
+    
+          if(!str.includes('+') && !str.includes('-') && !str.includes('*')&& !str.includes('/') && !str.includes('(') && !str.includes(')')){        
+            go = false;
+            arr.push(str);
+          }
+        }
+        if(arr[arr.length - 1] === ''){
+          arr.pop()
+        }
+          this.setState({
+            test : arr,
+          })
+          this.parensTest = arr
+      }
+      
+    parens = () => {
+        let arr = this.parensTest
+        let count = 0
+        let start = 0;
+        let stop = 0
+        for(let i = 0; i < arr.length;i += 1){
+          if(count !== 2 ){
+            if(arr[i] === '('){
+              start = i
+              count += 1
+            }else if(arr[i] === ')'){
+              stop = i
+              count += 1
+            }
+          }
+        }
+    
+        if(count === 2){
+          let newArr = arr.splice(start,stop - start + 1)
+          let newArr1 = newArr.splice(1,newArr.length - 2)
+          const newNum = this.totaling(newArr1)
+          arr.splice(start,0,newNum)
+        }
+        return this.parensTest
+      }
+
+  totaling = (input) => {
+    let arr = input;
+    let operations = ['*', '/', '+', '-'];
+    let go = true;
+    while(go){
+      for(let i = 0; i < arr.length; i += 1){
+        if(arr[i] === operations[0]){
+          const multiply = parseFloat(arr[i-1]) * parseFloat(arr[i+1]);
+          arr.splice(i-1,3);
+          arr.splice(i-1,0,multiply.toFixed(5));
+          i = 0;
+        }else if(arr[i] === operations[1]){
+          const divide = (parseFloat(arr[i-1]) / parseFloat(arr[i+1]));
+          arr.splice(i-1,3);
+          arr.splice(i-1,0,divide.toFixed(2));
+          i = 0;
+        }
+      }
+      if(!arr.includes(operations[0]) && !arr.includes(operations[1])){
+        go = false;
+        operations.shift();
+        operations.shift();
+      }
+    }
+      let run = true;
+      while(run){
+      for(let i = 0; i < arr.length; i += 1){
+        if(arr[i] === operations[0]){
+          const add = parseFloat(arr[i-1]) + parseFloat(arr[i+1]);
+          arr.splice(i-1,3);
+          arr.splice(i-1,0,add.toFixed(5));
+          i = 0;
+        }else if(arr[i] === operations[1]){
+          const subtract = parseFloat(arr[i-1]) - parseFloat(arr[i+1]);
+          arr.splice(i-1,3);
+          arr.splice(i-1,0, subtract.toFixed(5));
+          i = 0;
+        }
+      }
+      if(!arr.includes(operations[0]) && !arr.includes(operations[1])){
+        run = false;
+        operations.shift();
+        operations.shift();
+      }
+    }
+    this.setState({
+      total : arr[0],
+      show : true,
+    });
+    return arr[0]
+  }
+
     handlePi = () => {
         this.setState(prevState => ({
           display: prevState.display + 'PI'
